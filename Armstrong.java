da17917543763b3dd29cbdb72bd4e51876b9b344
731614105013 <gomathiammupk29@gmail.com>
2017-07-05 14:32:17 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,23 @@
+# armstrong
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Armstrong{  
+  public static void main(String[] args)  {  
+    Scanner sc=new Scanner(System.in);
+    int c=0,a,temp;  
+    int n;
+    n=sc.nextInt();  
+    temp=n;  
+    while(n>0)  
+    {  
+      a=n%10;  
+      n=n/10;  
+      c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("armstrong number");   
+    else  
+    System.out.println("Not armstrong number");  
+  }  
+}  
